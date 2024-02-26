5791f7c3f471e9217badd75d5c7a653e8dfac720
ulich <ulich@users.noreply.github.com>
2024-02-26 08:02:12 +0100

    use compact theme

--- src/main/kotlin/net/ulich/crm/view/main/MainView.kt
+++ src/main/kotlin/net/ulich/crm/view/main/MainView.kt
@@ -1,5 +1,6 @@
 package net.ulich.crm.view.main
 
+import com.vaadin.flow.component.dependency.JsModule
 import com.vaadin.flow.router.Route
 import io.jmix.flowui.app.main.StandardMainView
 import io.jmix.flowui.view.ViewController
@@ -8,4 +9,5 @@ import io.jmix.flowui.view.ViewDescriptor
 @Route("")
 @ViewController("MainView")
 @ViewDescriptor("main-view.xml")
+@JsModule("@vaadin/vaadin-lumo-styles/presets/compact.js")
 open class MainView : StandardMainView()

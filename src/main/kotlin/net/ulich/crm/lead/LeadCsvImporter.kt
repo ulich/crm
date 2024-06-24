fd285d44b22032def8375385b03947df8a16d651
ulich <ulich@users.noreply.github.com>
2024-06-24 20:40:50 +0200

    support more types of csv formats

--- src/main/kotlin/net/ulich/crm/lead/LeadCsvImporter.kt
+++ src/main/kotlin/net/ulich/crm/lead/LeadCsvImporter.kt
@@ -1,23 +1,48 @@
 package net.ulich.crm.lead
 
+import com.opencsv.CSVParser
+import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReaderBuilder
 import io.jmix.core.DataManager
 import net.ulich.crm.entity.Gender
 import net.ulich.crm.entity.Lead
 import org.springframework.stereotype.Component
+import java.io.StringReader
 
 @Component
 class LeadCsvImporter(private val dataManager: DataManager) {
 
     fun importFromCsv(csv: String): Lead? {
-        val lines = csv.split("\n")
+        val csvParsers = listOf(
+            csvParser(';'),
+            csvParser('\t'),
+            csvParser(','),
+        )
+
+        for (parser in csvParsers) {
+            val lead = parse(csv, parser)
+            if (lead != null) {
+                return lead
+            }
+        }
+
+        return null
+    }
+
+    private fun parse(csv: String, parser: CSVParser): Lead? {
+        val lines = CSVReaderBuilder(StringReader(csv))
+            .withCSVParser(parser)
+            .build()
+            .readAll()
+
         if (lines.size != 2) {
             return null
         }
 
-        val header = lines[0].split("\t")
-        val dataRow = lines[1].split("\t")
+        val header = lines[0]
+        val dataRow = lines[1]
 
-        if (header.size != dataRow.size) {
+        if (header.size < 2) {
             return null
         }
 
@@ -42,16 +67,22 @@ class LeadCsvImporter(private val dataManager: DataManager) {
         }
     }
 
+    private fun csvParser(separator: Char): CSVParser {
+        return CSVParserBuilder()
+            .withSeparator(separator)
+            .build()
+    }
+
     private fun toGender(data: MutableMap<String, String>): Gender? {
         val str = data.get("Anrede")
         if (str == null) {
-            return null;
+            return null
         }
 
         val gender = when (str.uppercase()) {
             "HERR" -> Gender.MR
             "FRAU" -> Gender.MRS
-            else -> null;
+            else -> null
         }
 
         if (gender != null) {
--- src/test/kotlin/net/ulich/crm/lead/LeadCsvImporterTest.kt
+++ src/test/kotlin/net/ulich/crm/lead/LeadCsvImporterTest.kt
@@ -21,7 +21,7 @@ class LeadCsvImporterTest {
     }
 
     @Test
-    fun testImportFromCsv() {
+    fun testImportFromTabCsv() {
         val lead = importer.importFromCsv(
             """
             Anfrage gestellt	ID	Firma	Anrede	Vorname	Nachname	Straße	PLZ	Stadt	Land	E-Mail	Rufnummer	Alternative Rufnummer	Mobil	Einsatzort	Anzahl Personen	Kommentar
@@ -47,6 +47,38 @@ class LeadCsvImporterTest {
                 Mobil: +49 174 12345
                 Einsatzort: Ladenlokal
                 Anzahl Personen: 100+
+                Kommentar: Allgemeine Rufnummer: 0333 132321
+                """.trimIndent()
+        )
+    }
+
+    @Test
+    fun testImportFromSemicolonCsv() {
+        val lead = importer.importFromCsv(
+            """
+            "Anfrage gestellt";ID;Firma;Vorname;Nachname;Straße;PLZ;Stadt;Land;E-Mail;Telefon;Einsatzort;"Anzahl Personen";Kommentar
+            "12.03.2024 09:18";136353;"Some Company GmbH";Max;Mustermann;"Hauptstraße 1";12345;Berlin;DE;max.muster@gmbh.de;"+49 123 456789";Ladenlokal;30-50;"Allgemeine Rufnummer: 0333 132321"
+            """.trimIndent()
+        )
+
+        assertThat(lead).isNotNull()
+        assertThat(lead?.getGender()).isNull()
+        assertThat(lead?.companyName).isEqualTo("Some Company GmbH")
+        assertThat(lead?.firstName).isEqualTo("Max")
+        assertThat(lead?.lastName).isEqualTo("Mustermann")
+        assertThat(lead?.street).isEqualTo("Hauptstraße 1")
+        assertThat(lead?.postCode).isEqualTo("12345")
+        assertThat(lead?.city).isEqualTo("Berlin")
+        assertThat(lead?.email).isEqualTo("max.muster@gmbh.de")
+        assertThat(lead?.phoneNumber).isEqualTo("+49 123 456789")
+        assertThat(lead?.alternativePhoneNumber).isNull()
+        assertThat(lead?.notes).isEqualTo(
+            """
+                Anfrage gestellt: 12.03.2024 09:18
+                ID: 136353
+                Land: DE
+                Einsatzort: Ladenlokal
+                Anzahl Personen: 30-50
                 Kommentar: Allgemeine Rufnummer: 0333 132321
                 """.trimIndent()
         )

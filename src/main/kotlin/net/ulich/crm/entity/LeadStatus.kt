592dbb2cc27cddf0561722f9312cfc1adccdf180
Timo Ulich <ulich@users.noreply.github.com>
2025-03-18 22:22:50 +0100

    Remove lead status values which are now covered with tags

--- src/main/kotlin/net/ulich/crm/entity/LeadStatus.kt
+++ src/main/kotlin/net/ulich/crm/entity/LeadStatus.kt
@@ -4,9 +4,6 @@ import io.jmix.core.metamodel.datatype.EnumClass
 
 enum class LeadStatus(private val id: String) : EnumClass<String> {
     NEW("NEW"),
-    DECISION_EXPECTED_SOON("DECISION_EXPECTED_SOON"),
-    DECISION_EXPECTED_LATER("DECISION_EXPECTED_LATER"),
-    NEWSLETTER_UNSUBSCRIBED("NEWSLETTER_UNSUBSCRIBED"),
     SUCCESS("SUCCESS"),
     CANCELED("CANCELED");
 
